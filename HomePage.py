415bf6dbbc8b5d60255754f4921bc7c82a690392
RBUSJainIndustries <rbusindustry@gmail.com>
2025-06-19 14:49:36 +0530

    Update HomePage.py

--- HomePage.py
+++ HomePage.py
@@ -237,7 +237,7 @@ These products are extensively used in both heavy-duty and day-to-day applicatio
 """)
 
 with col2:
-    st.image("Applications.png", use_container_width =True)
+    st.image("images/Applications.png", use_container_width =True)
 
 st.markdown("<hr>", unsafe_allow_html=True)
 
@@ -254,13 +254,13 @@ What sets us apart is not just the strength of our products but also the care wi
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.image("Packing1.JPG", caption="Precision Packaging", use_container_width=True)
+    st.image("images/Packing1.JPG", caption="Precision Packaging", use_container_width=True)
 
 with col2:
-    st.image("Packing2.jpg", caption="Ready for Dispatch", use_container_width =True)
+    st.image("images/Packing2.jpg", caption="Ready for Dispatch", use_container_width =True)
 
 with col3:
-    st.image("Packing3.JPG", caption="Quality Inspection", use_container_width =True)
+    st.image("images/Packing3.JPG", caption="Quality Inspection", use_container_width =True)
 
 
 # Inquiry Form
